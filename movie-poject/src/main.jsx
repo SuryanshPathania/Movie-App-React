eb5fac1e39d54bb18b472fdc066a7fdd601579e1
Suryansh Pathania <sp.ssdps@gmail.com>
2025-02-18 18:47:08 +0530

    update

--- movie-poject/src/main.jsx
+++ movie-poject/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store'; // Adjust the path as necessary
+import { store, persistor } from './redux/store'; 
 import './index.css';
 import App from './App.jsx';
 
